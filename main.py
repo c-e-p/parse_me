7e55c90c95c3e5f1f79116d68dc2f2263562ea44
C. E. P <c.elena.palmer@outlook.com>
2017-05-04 20:54:46 -0500

    CLI input + string concatenation
    Adding quiz input variable and concatenating the output string

--- main.py
+++ main.py
@@ -14,6 +14,7 @@ def build_quiz(question_count, question_store):
     strand_count = len(question_store.strands)
     strand_index = 0
     standard_last_used = {}
+    question_numbers = ""
     for i in range(0, question_count):
         #pick a strand id
         strand_pick = question_store.strands_list[strand_index]
@@ -23,23 +24,29 @@ def build_quiz(question_count, question_store):
             if standard_last_used[strand_pick] == len(standard_options) - 1:
                 standard_last_used[strand_pick] = 0
                 standard_pick = standard_options[0]
-                print(random.choice(question_store.standards[standard_pick].questions))
+                question_numbers += random.choice(question_store.standards[standard_pick].questions)
+                question_numbers += ","
             else:
                 #we still have more standards to pick!
                 standard_last_used[strand_pick] = standard_last_used[strand_pick] + 1
                 standard_pick = standard_options[standard_last_used[strand_pick]]
-                print(random.choice(question_store.standards[standard_pick].questions))
+                question_numbers += random.choice(question_store.standards[standard_pick].questions)
+                question_numbers += ","
         else:
             standard_pick = standard_options[0]
             standard_last_used[strand_pick] = 0
-            print(random.choice(question_store.standards[standard_pick].questions))
+            question_numbers += random.choice(question_store.standards[standard_pick].questions)
+            question_numbers += ","
         if strand_index == strand_count - 1:
             strand_index = 0
         else:
             strand_index = strand_index + 1
+    return question_numbers
 
 #main
 if __name__ == '__main__':
     question_store = question_store.QuestionStore()
     parse_csv(question_store)
-    build_quiz(10, question_store)
+    count = input("Enter the number of questions in the quiz: ")
+    question_numbers = build_quiz(int(count), question_store)
+    print(question_numbers[:-1])
